e492f7933d5ae1cdb78ea2e53805d73a1026086b
SeuCaiOo <caiopimentel8@gmail.com>
2019-03-11 11:56:54 -0300

    Add Layouts

---
+++ app/src/main/java/br/com/seucaio/testeicasei/ui/detail/VideoDetailActivity.kt
@@ -0,0 +1,13 @@
+package br.com.seucaio.testeicasei.ui.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.seucaio.testeicasei.R
+
+class VideoDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_detail)
+    }
+}
---
+++ app/src/main/java/br/com/seucaio/testeicasei/ui/list/VideoListActivity.kt
@@ -0,0 +1,13 @@
+package br.com.seucaio.testeicasei.ui.list
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.seucaio.testeicasei.R
+
+class VideoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_list)
+    }
+}
